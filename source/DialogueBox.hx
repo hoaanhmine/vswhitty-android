8821e2c476155101a2fb44186d5aaeefa2d95276
HMFNF <51531073+hoaanhmine@users.noreply.github.com>
2023-04-17 10:54:43 +0700

    update source/DialogueBox.hx

--- source/DialogueBox.hx
+++ source/DialogueBox.hx
@@ -278,6 +278,18 @@ class DialogueBox extends FlxSpriteGroup
 			dialogueStarted = true;
 		}
 
+   #if android
+var justTouched:Bool = false;
+
+for (touch in FlxG.touches.list)
+{
+        if (touch.justPressed)
+        {
+               justTouched = true;
+        }
+}
+#end
+
 		if (FlxG.keys.justPressed.ANY)
 		{
 			remove(dialogue);

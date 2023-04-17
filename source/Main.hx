7adc010a28e74933bcab71312cf3dfae018552fd
HMFNF <51531073+hoaanhmine@users.noreply.github.com>
2023-04-17 10:45:00 +0700

    update source/Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -19,7 +19,7 @@ class Main extends Sprite
 	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var initialState:Class<FlxState> = TitleState; // The FlxState the game starts with.
 	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
-	var framerate:Int = 120; // How many frames per second the game should run at.
+	var framerate:Int = 60; // How many frames per second the game should run at.
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
 
@@ -36,7 +36,6 @@ class Main extends Sprite
 	}
 
 	public function new()
-SUtil.uncaughtErrorHandler();
 	{
 		super();
 
@@ -73,21 +72,20 @@ SUtil.uncaughtErrorHandler();
 			gameWidth = Math.ceil(stageWidth / zoom);
 			gameHeight = Math.ceil(stageHeight / zoom);
 		}
-
+   SUtil.check();
 		#if !debug
 		initialState = TitleState;
 		#end
-    SUtil.checkFiles();
 		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
 
 		addChild(game);
 
-		#if !mobile
-		fpsCounter = new FPS(10, 3, 0xFFFFFF);
-		addChild(fpsCounter);
-		toggleFPS(FlxG.save.data.fps);
-
-		#end
+```haxe
+#if mobile
+fpsCounter = new FPS(10, 3, 0xFFFFFF);
+addChild(fpsCounter);
+toggleFPS(FlxG.save.data.fps);
+#end
 	}
 
 	var game:FlxGame;
